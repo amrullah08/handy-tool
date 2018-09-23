474e690f25366f43d70c65995fc32ea24ad7e0d8
syed sadakhath khalander <sadhakhath47@gmail.com>
2018-09-23 22:42:59 +0530

    readinsheet1

---
+++ FastWay INDIA/FastWayExcelReader/FastWayExcelReader/PriceInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWayExcelReader
+{
+    class PriceInfo
+    {
+       public string weight;
+       public string zone;
+       public string price;
+    }
+}
--- FastWay INDIA/FastWayExcelReader/FastWayExcelReader/Program.cs
+++ FastWay INDIA/FastWayExcelReader/FastWayExcelReader/Program.cs
@@ -42,6 +42,48 @@ namespace FastWayExcelReader
             {
                 Console.WriteLine("countryname = {0} countrycode = {1} code = {2}", zi.Countryname, zi.Countrycode, zi.zone);
             }
+
+            Excel exnew = new Excel(@"C:\Users\sadha\Pictures\BRV - DHL New Rates 19 Effect From March 2018.xlsx", 1);
+
+            List<PriceInfo> priceinfolist = new List<PriceInfo>();
+
+            for(int i = 6; i < 10; i++)
+            {
+                for(int j = 1; j <= 14; j++)
+                {
+                    PriceInfo pricein = new PriceInfo();
+                    pricein.weight = exnew.ReadCell(i, 0);
+                    pricein.price = exnew.ReadCell(i, j);
+                    pricein.zone = j.ToString();
+                    priceinfolist.Add(pricein);
+                }
+            }
+
+
+            for (int i = 12; i < 61; i++)
+            {
+                for (int j = 1; j <= 14; j++)
+                {
+                    PriceInfo pricein = new PriceInfo();
+                    pricein.weight = exnew.ReadCell(i, 0);
+                    pricein.price = exnew.ReadCell(i, j);
+                    pricein.zone = j.ToString();
+                    priceinfolist.Add(pricein);
+                }
+            }
+
+            for (int i = 64; i < 69; i++)
+            {
+                for (int j = 1; j <= 14; j++)
+                {
+                    PriceInfo pricein = new PriceInfo();
+                    pricein.weight = exnew.ReadCell(i, 0);
+                    pricein.price = exnew.ReadCell(i, j);
+                    pricein.zone = j.ToString();
+                    priceinfolist.Add(pricein);
+                }
+            }
+
         }
     }
 }
