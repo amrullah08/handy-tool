b8323f4cfcb8fd776943e90a0b04604048aaa2c0
syed sadakhath khalander <sadhakhath47@gmail.com>
2018-09-23 20:41:58 +0530

    com

--- FastWay INDIA/FastWayExcelReader/FastWayExcelReader/Program.cs
+++ FastWay INDIA/FastWayExcelReader/FastWayExcelReader/Program.cs
@@ -12,12 +12,36 @@ namespace FastWayExcelReader
         {
             Excel excel = new Excel(@"C:\Users\sadha\Pictures\BRV - DHL New Rates 19 Effect From March 2018.xlsx", 2);
 
-            Zoneinfo zone = new Zoneinfo();
+           
+            List<Zoneinfo> zoneinfolist = new List<Zoneinfo>();
 
-            zone.Countryname = excel.ReadCell(2, 0).ToString();
-            zone.Countrycode = excel.ReadCell(2, 1);
-            zone.zone = excel.ReadCell(2, 2);
-            Console.WriteLine("countryname = {0} countrycode={1} code = {2}", zone.Countryname, zone.Countrycode, zone.zone);
+           
+
+            for(int i = 2; i < 118; i++)
+            {
+                Zoneinfo excelzone = new Zoneinfo();
+
+                excelzone.Countryname = excel.ReadCell(i, 0).ToString();
+                excelzone.Countrycode = excel.ReadCell(i, 1);
+                excelzone.zone = excel.ReadCell(i, 2);
+
+                zoneinfolist.Add(excelzone);
+            }
+
+            for(int j = 2; j < 118; j++)
+            {
+                Zoneinfo excelzone = new Zoneinfo();
+
+                excelzone.Countryname = excel.ReadCell(j, 3).ToString();
+                excelzone.Countrycode = excel.ReadCell(j, 4);
+                excelzone.zone = excel.ReadCell(j, 5);
+
+                zoneinfolist.Add(excelzone);
+            }
+            foreach (var zi in zoneinfolist)
+            {
+                Console.WriteLine("countryname = {0} countrycode = {1} code = {2}", zi.Countryname, zi.Countrycode, zi.zone);
+            }
         }
     }
 }
