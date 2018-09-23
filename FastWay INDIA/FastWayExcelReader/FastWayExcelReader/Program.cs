5a96ba27e44c739addf39a9e67b726d5e3403849
syed sadakhath khalander <sadhakhath47@gmail.com>
2018-09-23 19:26:42 +0530

    fastway initial excel reading

---
+++ FastWay INDIA/FastWayExcelReader/FastWayExcelReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWayExcelReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel excel = new Excel(@"C:\Users\sadha\Pictures\BRV - DHL New Rates 19 Effect From March 2018.xlsx", 2);
+
+            Zoneinfo zone = new Zoneinfo();
+
+            zone.Countryname = excel.ReadCell(2, 0).ToString();
+            zone.Countrycode = excel.ReadCell(2, 1);
+            zone.zone = excel.ReadCell(2, 2);
+            Console.WriteLine("countryname = {0} countrycode={1} code = {2}", zone.Countryname, zone.Countrycode, zone.zone);
+        }
+    }
+}
---
+++ FastWay INDIA/FastWayExcelReader/FastWayExcelReader/Zoneinfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWayExcelReader
+{
+    class Zoneinfo
+    {
+       public string Countryname;
+       public string Countrycode;
+       public string zone;
+    }
+    
+
+}
